bb0dc9a19a1997852b44424a2b8487ae882756f2
yeonjy <yeonjypaper@gmail.com>
2025-01-17 22:36:14 +0900

    feat: BOJ / 1541번 / 실버2

---
+++ yeonjy/boj/BOJ_1541.java
@@ -0,0 +1,27 @@
+package yeonjy.boj;
+
+import java.util.Scanner;
+
+public class BOJ_1541 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        String[] arr = input.split("-");
+        int total = 0;
+
+        String[] first = arr[0].split("\\+");
+        for (String num : first) {
+            total += Integer.parseInt(num);
+        }
+
+        for (int i = 1; i < arr.length; i++) {
+            String[] nums = arr[i].split("\\+");
+            for (String num : nums) {
+                total -= Integer.parseInt(num);
+            }
+        }
+
+        System.out.println(total);
+    }
+}
