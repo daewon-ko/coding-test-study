a00333d14e55b703a487ca7750197a961082f9ea
Jo Myeong-geun <mg6359@naver.com>
2025-01-18 23:19:25 +0900

    feat: BOJ / 1012번 / 실버2

---
+++ wda067/boj/BOJ_1012.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+백준 / 유기농 배추 / 실버2
+https://www.acmicpc.net/problem/1012
+ */
+public class BOJ_1012 {
+
+    static final int[] dr = {0, 1, 0, -1};
+    static final int[] dc = {1, 0, -1, 0};
+    static boolean[][] visit;
+    static int[][] map;
+    static int count, M, N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int testCase = 0; testCase < T; testCase++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken()); //가로
+            N = Integer.parseInt(st.nextToken()); //세로
+            int K = Integer.parseInt(st.nextToken()); //배추 개수
+            visit = new boolean[N][M];
+            map = new int[N][M];
+            count = 0;
+
+            for (int cabbage = 0; cabbage < K; cabbage++) {
+                st = new StringTokenizer(br.readLine());
+                int X = Integer.parseInt(st.nextToken());
+                int Y = Integer.parseInt(st.nextToken());
+                map[Y][X] = 1;
+            }
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+
+                    if (map[i][j] == 1 && !visit[i][j]) {
+                        dfs(i, j);
+                        count++;
+                    }
+                }
+            }
+
+            System.out.println(count);
+        }
+    }
+
+    static void dfs(int r, int c) {
+        visit[r][c] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nextR = r + dr[i];
+            int nextC = c + dc[i];
+
+            if (nextR < 0 || nextR >= N || nextC < 0 || nextC >= M) {
+                continue;
+            }
+
+            if (map[nextR][nextC] == 1 && !visit[nextR][nextC]) {
+                dfs(nextR, nextC);
+            }
+
+        }
+    }
+}
