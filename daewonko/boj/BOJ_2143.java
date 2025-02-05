1c641bb4ab56680dce7c6cfbbb94eb01e0ba5dfe
pinetree <eodnjs568@gmail.com>
2025-02-05 22:37:39 +0900

    feat : BOJ / 2143 / 골드3

--- daewonko/boj/BOJ_2143.java
+++ daewonko/boj/BOJ_2143.java
@@ -3,6 +3,7 @@ package daewonko.boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -16,10 +17,24 @@ public class BOJ_2143 {
     static Long [] A, B;
     static List<Long> arr = new ArrayList<>();
     static List<Long> brr = new ArrayList<>();
+=======
+import java.util.StringTokenizer;
+
+public class BOJ_2143 {
+
+
+    //백준 두 배열의 합
+    static Long t;
+    static int n, m;
+    static Long[] arr;
+    static Long[] brr;
+
+>>>>>>> 1148dd0 (feat : BOJ / 2143 / 골드3)
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         t = Long.parseLong(br.readLine());
         n = Integer.parseInt(br.readLine());
+<<<<<<< HEAD
         A = new Long[n];
         StringTokenizer st = null;
         st = new StringTokenizer(br.readLine());
@@ -61,10 +76,24 @@ public class BOJ_2143 {
         System.out.println(count);
 
 
+=======
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+
+        m = Integer.parseInt(br.readLine());
+        for (int i = 0; i < m; i++) {
+            brr[i] = Long.parseLong(st.nextToken());
+        }
+
+        backtracking(0L);
+>>>>>>> 1148dd0 (feat : BOJ / 2143 / 골드3)
 
 
     }
 
+<<<<<<< HEAD
     // target보다 커지는 처음의 수를 찾는다.
     static int upperBound(List<Long> list, long target) {
         int left = 0;
@@ -95,3 +124,28 @@ public class BOJ_2143 {
         return left;
     }
 }
+=======
+    // sum이 t여야한다.
+    // t는 -10억에서 +10억까지이다.
+    // 일반적인 백트래킹은 인덱스와 값이 일치하는 양의정수의 값을 구하는 문제인데,
+    // 해당문제는 t가 음의 정수가 될 수도 있고 심지어 그 값의 범위가 크다.
+    // 따라서 visited 배열을 이용할 수 없을 것으로 보인다.
+    public static void backtracking(Long sum) {
+        if (sum == t) {
+            //A와 B의 연속된 부배열이 존재하는지 확인
+            // 한 개 이상의 원소를 지닌 연속된 부분집합을 더했을때 ,
+            // 각 arr과 brr에서 더해서 만들 수 있는지를 확인하고 있다면 cnt ++
+            // O(N)
+            // 구하고자 하는 부분집합의 값이 무엇인지 알 수가 없지 않을까?
+
+
+            // O(M)
+
+        }
+
+
+    }
+}
+
+
+>>>>>>> 1148dd0 (feat : BOJ / 2143 / 골드3)
