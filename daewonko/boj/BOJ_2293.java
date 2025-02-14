40d7dc212eee9929c8a281e71a4afdee92e9409a
pinetree <eodnjs568@gmail.com>
2025-02-14 20:10:59 +0900

    feat: BOJ / 2293 / 골드4

---
+++ daewonko/boj/BOJ_2293.java
@@ -0,0 +1,29 @@
+package daewonko.boj;
+import java.util.*;
+public class BOJ_2293 {
+
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] coins = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                coins[i] = sc.nextInt();
+            }
+
+            int[] dp = new int[k + 1];
+            dp[0] = 1;
+
+            for (int coin : coins) {
+                for (int i = coin; i <= k; i++) {
+                    dp[i] += dp[i - coin];
+                }
+            }
+
+            System.out.println(dp[k]);
+        }
+    }
+
