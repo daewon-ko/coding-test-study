248846d9478f8f104bab0d99f6b6127053b0e13f
yeonjy <81320703+yeonjy@users.noreply.github.com>
2025-02-26 22:10:23 +0900

    feat: BOJ / 1915번 / 골드4

---
+++ yeonjy/boj/BOJ_1915.java
@@ -0,0 +1,29 @@
+package yeonjy.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1915 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[n + 1][m + 1];
+        int max = 0;
+
+        for (int i = 1; i <= n; i++) {
+            String line = br.readLine();
+            for (int j = 1; j <= m; j++) {
+                if (line.charAt(j - 1) == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+
+        System.out.println(max * max);
+    }
+}
