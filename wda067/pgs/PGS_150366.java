8670af6689a787aa940c41d7b23312bed042aa7c
Jo Myeong-geun <mg6359@naver.com>
2025-06-02 03:01:38 +0900

    chore: PGS / 150366번 / Level 3

--- wda067/pgs/PGS_150366.java
+++ wda067/pgs/PGS_150366.java
@@ -7,100 +7,106 @@ https://school.programmers.co.kr/learn/courses/30/lessons/150366
  */
 class PGS_150366 {
 
-    private static final int SIZE = 50;
-    private static final int TOTAL = SIZE * SIZE;
-    private int[] parent = new int[TOTAL];
-    private String[] values = new String[TOTAL];
+    int SIZE = 50 * 50 + 1;
+    int[] parent = new int[SIZE];
+    String[] value = new String[SIZE];
 
-    public String[] solution(String[] commands) {
+    public List<String> solution(String[] commands) {
+        for (int i = 1; i < SIZE; i++) {
+            parent[i] = i;
+            value[i] = "";
+        }
+
         List<String> answer = new ArrayList<>();
 
-        for (String cmd : commands) {
-            String[] parts = cmd.split(" ");
-            String action = parts[0];
+        for (String s : commands) {
+            String[] split = s.split(" ");
+            String command = split[0];
 
-            switch (action) {
-                case "UPDATE":
-                    if (parts.length == 4) {
-                        int r = Integer.parseInt(parts[1]) - 1;
-                        int c = Integer.parseInt(parts[2]) - 1;
-                        String value = parts[3];
-                        int idx = r * SIZE + c;
-                        int root = find(idx);
-                        values[root] = value;
-                    } else {
-                        String oldValue = parts[1];
-                        String newValue = parts[2];
-                        for (int i = 0; i < TOTAL; i++) {
-                            if (values[i].equals(oldValue)) {
-                                values[i] = newValue;
-                            }
-                        }
-                    }
-                    break;
+            if (command.equals("UPDATE")) {
+                if (split.length == 4) {  //"UPDATE r c value"
+                    int r = Integer.parseInt(split[1]);
+                    int c = Integer.parseInt(split[2]);
+                    String val = split[3];
 
-                case "MERGE":
-                    int r1 = Integer.parseInt(parts[1]) - 1;
-                    int c1 = Integer.parseInt(parts[2]) - 1;
-                    int r2 = Integer.parseInt(parts[3]) - 1;
-                    int c2 = Integer.parseInt(parts[4]) - 1;
-                    int idx1 = r1 * SIZE + c1;
-                    int idx2 = r2 * SIZE + c2;
-                    union(idx1, idx2);
-                    break;
-
-                case "UNMERGE":
-                    int r = Integer.parseInt(parts[1]) - 1;
-                    int c = Integer.parseInt(parts[2]) - 1;
-                    int idx = r * SIZE + c;
+                    //2차원을 1차원으로 표현
+                    //(1, 1) -> 1
+                    //(2, 1) -> 51
+                    int idx = (r - 1) * 50 + c;
                     int root = find(idx);
-                    String val = values[root];
-                    List<Integer> toReset = new ArrayList<>();
-                    for (int i = 0; i < TOTAL; i++) {
-                        if (find(i) == root) {
-                            toReset.add(i);
+                    value[root] = val;
+                } else {  //"UPDATE value1 value2"
+                    String val1 = split[1];
+                    String val2 = split[2];
+
+                    for (int i = 1; i < SIZE; i++) {
+                        if (value[i].equals(val1)) {
+                            value[i] = val2;
                         }
                     }
-                    for (int i : toReset) {
-                        parent[i] = i;
-                        values[i] = "";
-                    }
-                    values[idx] = val;
-                    break;
+                }
+            } else if (command.equals("MERGE")) {  //"MERGE r1 c1 r2 c2"
+                int r1 = Integer.parseInt(split[1]);
+                int c1 = Integer.parseInt(split[2]);
+                int r2 = Integer.parseInt(split[3]);
+                int c2 = Integer.parseInt(split[4]);
 
-                case "PRINT":
-                    r = Integer.parseInt(parts[1]) - 1;
-                    c = Integer.parseInt(parts[2]) - 1;
-                    idx = r * SIZE + c;
-                    root = find(idx);
-                    String result = values[root];
-                    answer.add(result.isEmpty() ? "EMPTY" : result);
-                    break;
+                int idx1 = (r1 - 1) * 50 + c1;
+                int idx2 = (r2 - 1) * 50 + c2;
+                union(idx1, idx2);
+            } else if (command.equals("UNMERGE")) {  //"UNMERGE r c"
+                int r = Integer.parseInt(split[1]);
+                int c = Integer.parseInt(split[2]);
+                int idx = (r - 1) * 50 + c;
+                int root = find(idx);
+                String val = value[root];
+
+                //병합을 해제할 셀 탐색
+                List<Integer> toUnmerge = new ArrayList<>();
+                for (int i = 1; i < SIZE; i++) {
+                    if (find(i) == root) {
+                        toUnmerge.add(i);
+                    }
+                }
+
+                for (int i : toUnmerge) {
+                    parent[i] = i;
+                    value[i] = "";
+                }
+                value[idx] = val;
+            } else if (command.equals("PRINT")) {  //"PRINT r c"
+                int r = Integer.parseInt(split[1]);
+                int c = Integer.parseInt(split[2]);
+                int idx = (r - 1) * 50 + c;
+                int root = find(idx);
+                String val = value[root];
+
+                answer.add(val.isEmpty() ? "EMPTY" : val);
             }
         }
 
-        return answer.toArray(new String[0]);
+        return answer;
     }
 
-    private int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]);
+    int find(int x) {
+        if (parent[x] == x) {
+            return x;
         }
-        return parent[x];
+        return parent[x] = find(parent[x]);
     }
 
-    private void union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-        if (rootX == rootY) {
+    void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a == b) {
             return;
         }
 
-        //병합 시 우선순위: 값이 있는 셀을 대표로 설정
-        if (!values[rootX].isEmpty()) {
-            parent[rootY] = rootX;
+        if (value[a].isEmpty()) {
+            parent[a] = b;
         } else {
-            parent[rootX] = rootY;
+            parent[b] = a;
         }
     }
 }
