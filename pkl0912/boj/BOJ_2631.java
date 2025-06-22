c2c0f10f91da7daf76020b034f125c22dc92f26a
pkl0912 <pkl0912@naver.com>
2025-06-22 22:53:21 +0900

    feat: BOJ / 2631번 / 골드4

---
+++ pkl0912/boj/BOJ_2631.java
@@ -0,0 +1,29 @@
+package pkl0912.boj;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_2631 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int max = 0;
+        for(int i = 0; i<n; i++){
+            dp[i] = 1;
+            for(int j = 0; j<i; j++){
+                if(arr[j]<arr[i]){
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+            
+        }
+        System.out.println(n-max);
+    }
+    
+}
